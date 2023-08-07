ecd4dc8ad68fffb0e8d21822ff967f351560fbf6
MK <m@mad-ears.de>
2023-08-07 15:28:01 +0200

    According to tutorial.

--- src/Providers/ThemeServiceProvider.php
+++ src/Providers/ThemeServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace Theme\Providers;
 
+use IO\Helper\TemplateContainer;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
 
 class ThemeServiceProvider extends ServiceProvider
 {
@@ -13,5 +16,17 @@ class ThemeServiceProvider extends ServiceProvider
     public function register()
     {
 
+    }
+
+    /**
+     * Boot a template for the basket that will be displayed in the template plugin instead of the original basket.
+     */
+    public function boot(Twig $twig, Dispatcher $eventDispatcher)
+    {
+        $eventDispatcher->listen('IO.tpl.basket', function(TemplateContainer $container, $templateData)
+        {
+            $container->setTemplate('Theme::content.ThemeBasket');
+            return false;
+        }, 0);
     }
 }
