4e89aa146144f6b5a1a661ed9cea24cbc0ab35a9
MK <m@mad-ears.de>
2023-08-07 16:06:28 +0200

    According to tutorial.

--- src/Providers/ThemeServiceProvider.php
+++ src/Providers/ThemeServiceProvider.php
@@ -2,10 +2,15 @@
 
 namespace Theme\Providers;
 
+use IO\Helper\TemplateContainer;
+use IO\Helper\ComponentContainer;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
 
 class ThemeServiceProvider extends ServiceProvider
 {
+  const PRIORITY = 0;
 
     /**
      * Register the service provider.
@@ -13,5 +18,19 @@ class ThemeServiceProvider extends ServiceProvider
     public function register()
     {
 
+    }
+
+    /**
+     * Boot a template for the basket that will be displayed in the template plugin instead of the original basket.
+     */
+    public function boot(Twig $twig, Dispatcher $eventDispatcher, ConfigRepository $config)
+    {
+        $eventDispatcher->listen("IO.Resources.Import", function (ResourceContainer $container)
+        {
+            if ($container->getOriginComponentTemplate()=='Ceres::Item.Components.SingleItem')
+            {
+                $container->addScriptTemplate('Theme::content.SingleItem');
+            }
+        }, self::PRIORITY);
     }
 }
